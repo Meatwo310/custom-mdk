02ab91deb4f044ff42bd692e73758ea28cdfa26c
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-09-01 23:33:33 +0900

    サーバーコンフィグをコメントアウト状態で追加

--- src/main/java/io/github/meatwo310/examplemod/ExampleMod.java
+++ src/main/java/io/github/meatwo310/examplemod/ExampleMod.java
@@ -1,6 +1,8 @@
 package io.github.meatwo310.examplemod;
 
+import io.github.meatwo310.examplemod.config.ServerConfig;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ExampleMod.MODID)
@@ -8,5 +10,6 @@ public class ExampleMod {
     public static final String MODID = "examplemod";
 
     public ExampleMod(FMLJavaModLoadingContext ctx) {
+//        ctx.registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC);
     }
 }
