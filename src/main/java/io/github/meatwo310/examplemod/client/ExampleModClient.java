cc73fddb3a000dc96c117e4a76810552724e9e8c
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-11-25 14:13:49 +0900

    コンフィグのタイトルを指定できなさそうなので一旦コメントアウト

--- src/main/java/io/github/meatwo310/examplemod/client/ExampleModClient.java
+++ src/main/java/io/github/meatwo310/examplemod/client/ExampleModClient.java
@@ -4,12 +4,10 @@ import io.github.meatwo310.examplemod.ExampleMod;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
 public class ExampleModClient {
     public ExampleModClient(ModContainer container) {
-        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+//        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 }
