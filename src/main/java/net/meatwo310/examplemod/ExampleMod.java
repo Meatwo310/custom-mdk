90d678a5f525297e01a97a091f30435b336e4fa2
Meatwo310 <git@meatwo310.net>
2026-03-24 21:36:17 +0900

    feat: locユーティリティメソッドを追加

--- src/main/java/net/meatwo310/examplemod/ExampleMod.java
+++ src/main/java/net/meatwo310/examplemod/ExampleMod.java
@@ -1,5 +1,6 @@
 package net.meatwo310.examplemod;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -9,5 +10,14 @@ public class ExampleMod {
 
     public ExampleMod(FMLJavaModLoadingContext ctx) {
 //        ctx.registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC);
+    }
+
+    /**
+     * Utility method to create a {@link ResourceLocation} with the namespace of this mod.
+     * @param path Path of the resource. Example: {@code "example_item"}
+     * @return {@link ResourceLocation} with the namespace of this mod and the given path. Example: {@code examplemod:example_item}
+     */
+    public static ResourceLocation loc(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
     }
 }
